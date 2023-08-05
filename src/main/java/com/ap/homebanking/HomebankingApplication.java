969a1881ded253a28ab54a58c4c8f2920de8d1cd
Maximiliano Camargo <maxi.camargo@gmail.com>
2023-08-04 23:18:23 -0300

    Se realizaron las actividades solicitadas en la TASK1

--- src/main/java/com/ap/homebanking/HomebankingApplication.java
+++ src/main/java/com/ap/homebanking/HomebankingApplication.java
@@ -1,13 +1,30 @@
 package com.ap.homebanking;
 
+import com.ap.homebanking.models.Client;
+import com.ap.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args -> {
+
+		Client client1 = new Client("Melba","Morel","melba@mindhub.com");
+		Client client2 = new Client("Maximiliano","Camargo","maxi.camargo@gmail.com");
+
+		clientRepository.save(client1);
+		clientRepository.save(client2);
+
+		});
 	}
 
 }
